8562f270b968414534a9e5a84bdb69cd0d71e88d
Gabriel Marques <gabriel_frahm@hotmail.com>
2021-08-10 17:30:58 -0300

    ✨ new interfaces

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Accessory } from '../../components/Accessory';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
 
 import {
   Container,
   Header,
-  CarImages
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Accessories,
+  About,
+  Footer,
 } from './styles';
+import { Button } from '../../components/Button';
 
 export function CarDetails() {
   return (
@@ -21,6 +42,44 @@ export function CarDetails() {
           ImageUrl={['https://cdn.picpng.com/audi/audi-face-28582.png']}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Audi</Brand>
+            <Name>carro</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasoline" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+
+        </Accessories>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado
+          na praça Real Maestraza de Seville. É um belíssimo carro para quem gosta
+          de acelerar
+        </About>
+      </Content>
+
+      <Footer>
+        <Button
+          title="Agendar"
+          color="green"
+        />
+      </Footer>
+
     </Container>
   );
 }
