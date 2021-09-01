ff635a3c49133da6595259dcf4a8f1f1b7de3cff
Gabriel Marques <gabriel_frahm@hotmail.com>
2021-09-01 18:05:50 -0300

    feature: animated button home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,11 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Ionicons } from '@expo/vector-icons'
 import { useTheme } from 'styled-components';
+import { StyleSheet } from 'react-native';
+import { RectButton, PanGestureHandler } from 'react-native-gesture-handler';
+
+import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+const ButtonAnimated = Animated.createAnimatedComponent(RectButton)
 
 import Logo from '../../assets/logo.svg';
 import { api } from '../../service/api';
@@ -21,17 +26,46 @@ import {
 } from './styles';
 
 export function Home() {
+
+
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const navigation = useNavigation();
   const theme = useTheme();
+
+  const positionY = useSharedValue(0);
+  const positionX = useSharedValue(0);
+
+  const myCarsButtonStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateY: positionY.value },
+        { translateX: positionX.value },
+      ]
+    }
+  });
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, ctx: any){
+      ctx.positionX = positionX.value;
+      ctx.positionY = positionY.value;
+    },
+    onActive(event, ctx: any){
+      positionX.value = ctx.positionX + event.translationX;
+      positionY.value = ctx.positionY + event.translationY;
+    },
+    onEnd(){
+      positionX.value = withSpring(0);
+      positionY.value = withSpring(0);
+    }
+  });
 
   function handleCarDetails(car: CarDTO) {
     // erro for typescript but, issus open in github
     navigation.navigate('CarDetails', { car });
   }
 
-  function handleOpenMyCars(){
+  function handleOpenMyCars() {
     // erro for typescript but, issus open in github
     navigation.navigate('MyCars');
   }
@@ -75,14 +109,35 @@ export function Home() {
             renderItem={({ item }) => <Car data={item} onPress={() => handleCarDetails(item)} />}
           />
       }
-
-      <MyCarsButton onPress={handleOpenMyCars}>
-        <Ionicons
-          name="ios-car-sport"
-          size={32}
-          color={theme.colors.shape}
-        />
-      </MyCarsButton>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[myCarsButtonStyle,
+            {
+              position: 'absolute',
+              bottom: 13,
+              right: 22,
+            }
+          ]}
+        >
+          <ButtonAnimated onPress={handleOpenMyCars} style={[styles.button, { backgroundColor: theme.colors.main }]}>
+            <Ionicons
+              name="ios-car-sport"
+              size={32}
+              color={theme.colors.shape}
+            />
+          </ButtonAnimated>
+        </Animated.View>
+      </PanGestureHandler>
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
